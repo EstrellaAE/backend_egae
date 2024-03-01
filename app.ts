aa39aea337f51dad60d6f9feb143fbee2fd2c5ae
EstrellaAE <eaguayoexiga@gmail.com>
2024-03-01 14:34:40 -0600

    actualizacion 01/03/2024

---
+++ app.ts
@@ -0,0 +1,57 @@
+
+import authRoutes from './src/routes/authRoutes';
+import express, { Application } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+
+/*
+* Clase de inicio de nuestra aplicación NodeJsExpress
+* Autor: Estrella Guadalupe Aguayo Exiga
+* Fecha: 01/02/04
+*/
+class Server {
+    private app: Application;
+
+
+    //Inicializa clase
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+        this.app.listen(this.app.get("port"), () => {
+            console.log("Server on port", this.app.get("port"));
+        });
+    }
+
+
+    //Configuración de módulos
+    config(): void {
+        // configuración del puerto para el servidor
+        this.app.set("port", 3000);
+
+
+        // muestra las peticiones en consola
+        this.app.use(morgan("dev"));
+
+
+        // puertos de conexión de la API
+        this.app.use(cors());
+
+
+        // solo se permiten peticiones en formato JSON
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false, }),
+        );
+    }
+
+
+    //Configura las rutas
+    routes() {
+        this.app.use("/", authRoutes);
+    }
+}
+
+
+const server = new Server();
---
+++ dist/src/routes/authRoutes.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+/*
+*Clase para funcionalidad de rutas Login
+*/
+class AuthRoutes {
+    //Inicializa
+    constructor() {
+        this.router = (0, express_1.Router)();
+        this.config();
+    }
+    config() {
+        this.router.get('/', (req, res) => {
+            res.send('Invocando Autenticacion');
+        });
+    }
+}
+const authRoutes = new AuthRoutes();
+exports.default = authRoutes.router;
+//# sourceMappingURL=authRoutes.js.map
